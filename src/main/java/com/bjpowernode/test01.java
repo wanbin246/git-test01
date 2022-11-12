5b459244d01c6f9065e07c855115c9ff300d085a
wangbin <wangbin@163.com>
2022-11-12 23:16:04 +0800

    添加hello文件

---
+++ src/main/java/com/bjpowernode/test01.java
@@ -0,0 +1,15 @@
+package com.bjpowernode;
+
+/**
+ * wangbin
+ * 2022.11.12
+ */
+public class test01 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("dfsd");
+        System.out.println("dfsd");
+    }
+}
